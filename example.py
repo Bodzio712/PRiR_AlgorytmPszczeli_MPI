ab4647826ae918f2650883c011b2eaa0b5de2356
cetlinsb <cetlinskib@gmail.com>
2019-06-11 20:08:12 +0200

    Porównanie działania i końcowa wersja

--- example.py
+++ example.py
@@ -1,9 +1,10 @@
 import SwarmPackagePy
 from SwarmPackagePy import testFunctions as tf
 from SwarmPackagePy import animation, animation3D
+from SwarmPackagePy import aba
 
+aba = aba.aba(100, tf.cross_in_tray_function, -10, 10, 2, 1000)
+#animation(aba.get_agents(), tf.cross_in_tray_function, -10, 10)
+#animation3D(aba.get_agents(), tf.cross_in_tray_function, -10, 10)
 
-aba = SwarmPackagePy.aba(50, tf.easom_function, -10, 10, 2, 1000)
-animation(aba.get_agents(), tf.easom_function, -10, 10)
-animation3D(aba.get_agents(), tf.easom_function, -10, 10)
-
+print(aba.get_Gbest())
--- pfs.py
+++ pfs.py
@@ -1,57 +1,52 @@
 from mpi4py import MPI
-import SwarmPackagePy
 from SwarmPackagePy import aba
 from SwarmPackagePy import testFunctions
+import sys
+import os
 
+def sort(table,function):
+    change = 1
+    while change > 0:
+        change = 0
+        for i in range(table.__len__()-1):
+            if function(table[i])<function(table[i+1]):
+                tmp = table[i]
+                table[i] = table[i+1]
+                table[i+1] = tmp
 
-#def cross_in_tray_function(x):
-#    return round(-0.0001*(abs(sin(x[0])*sin(x[1])*exp(abs(100 -
-#                            sqrt(sum([i**2 for i in x]))/pi))) + 1)**0.1, 7)
+func = testFunctions.easom_function
 
-class MultiSwarms():
-    def __init__(self,n_bees,p_threads,f_function,lb, ub, dimension, iteration):
-        n = n_bees//p_threads
-        n_rest = n_bees%p_threads
-
-        for i in range(n_rest):
-            pass
-            #bees(n+1,f_function,lb, ub, dimension, iteration)
-        for i in range(p_threads-n_rest):
-            pass
-            #bees(n+1,f_function,lb, ub, dimension, iteration)
-        pass
-
-class xd():
-
-    def __init__(self,function):
-        self.__function = function
-
-    def fun(self,x):
-        return self.__function(x)
-
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-p_threads = 4
-
-if rank == 0:
-    data = {
-        'n': 7,
-        'function': testFunctions.cross_in_tray_function,
-        'type': 'only_swarms',#'stricted_swarms' - other type
-        'lb': -10,
-        'ub': 10,
-        "dim": 3,
-        'iterations': 100
-    }
-    for i in range(1,p_threads):
-        print(i)
-        comm.send(data, dest=i, tag=11)
-        print(comm.recv(source=i,tag=12))
-
-
-elif rank > 0:
-    data = comm.recv(source=0, tag=11)
-    optimizer = aba.aba(data['n'],data['function'], data['lb'], data['ub'], data['dim'], data['iterations'])
-    comm.send(optimizer.get_Gbest(),dest=0,tag=12)
-
-
+if sys.argv.__len__()==4:
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    p_threads = size
+    if rank == 0:
+        time_start = MPI.Wtime()
+        n_bees = int(sys.argv[2])
+        iterations = int(sys.argv[3])
+        data2 = {
+            'n': (n_bees // (p_threads-1)) +1,
+            'function': func,
+            'lb': -10,
+            'ub': 10,
+            "dim": 2,
+            'iterations': iterations
+        }
+        out = []
+        for i in range(1, p_threads):
+            comm.send(data2, dest=i, tag=11)
+        for i in range(1, p_threads):
+            out.append(comm.recv(source=i,tag=12))
+        sort(out,func)
+        print("Global best: " + out[0].__str__())
+        print("execution time: " + (MPI.Wtime()-time_start).__str__())
+    elif (rank > 0) & (rank<p_threads) :
+        data = comm.recv(source=0, tag=11)
+        optimizer = aba.aba(data['n'], data['function'], data['lb'], data['ub'], data['dim'], data['iterations'])
+        comm.send(optimizer.get_Gbest(), dest=0, tag=12)
+elif sys.argv.__len__()==3:
+    start = os.times().user
+    optimizer = aba.aba(int(sys.argv[1]),func,-10,10,2,int(sys.argv[2]))
+    print(optimizer.get_Gbest())
+    print("execution time: " + (os.times().user - start).__str__())
